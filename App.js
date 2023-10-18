4f746e469c61d8740d1f3f4fca53dab1887e4541
vidhyakolath <140418280+vidhyakolath@users.noreply.github.com>
2023-10-18 23:38:30 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Signin from './Components/Signin/Signin';
+import Signup from './Components/Signup/Signup.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contact from './Components/Contact/Contact';
+import Fetchexceldata from './Components/Excel/Fetchexceldata';
+import Product from './Components/Header/Product';
+
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Signin/>}/>
+      <Route path='/Signup' element={<Signup/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+      <Route path='/Excel' element={<Product/>}/>
+     </Routes>
+     </BrowserRouter>
+     
+    </div>
+  );
+}
+
+export default App;
